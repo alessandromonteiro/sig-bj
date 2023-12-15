9b0d45da098e014e4507986a00bbd6a3b61c4857
ALESSANDRO MONTEIRO <alessandromonteiro1975@gmail.com>
2023-12-15 00:16:03 -0300

    Initial Version

---
+++ sig-bj/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace sig_bj.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
